01dde235f27851383ee207234a93353e02fdd535
K.M Ahnaf Zamil <ahnafzamil896@gmail.com>
2021-07-13 14:57:06 +0600

    Completed starboard

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -6,7 +6,6 @@ import discord
 import os
 import datetime
 
-
 logger = getLogger(__name__)
 
 starboard_emoji = "⭐"
@@ -48,6 +47,7 @@ class Starboard(commands.Cog):
             "stars": stars,
         }
         await self.collection.insert_one(payload)
+        logger.info(f"Added message with ID {message.id} to starboard")
 
     async def _update_starboard(self, payload, message: discord.Message, stars: int):
         # Updating DB
@@ -70,6 +70,21 @@ class Starboard(commands.Cog):
             msg.guild.text_channels, id=int(payload["channel_id"])
         )
         await msg.edit(content=f":star: **{stars}** | {starred_msg_channel.mention}")
+        logger.info("Updated message with ID {message.id} in starboard")
+
+    async def _remove_from_starboard(self, payload, message: discord.Message):
+        # Deleting from DB
+        await self.collection.delete_one({"message_id": message.id})
+
+        # Deleting starboard message
+        starboard_channel = discord.utils.get(
+            message.guild.text_channels, name="starboard"
+        )
+        starred_msg = await starboard_channel.fetch_message(
+            payload["starboard_message_id"]
+        )
+        await starred_msg.delete()
+        logger.info(f"Removed message with ID {message.id} from starboard")
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, event: discord.RawReactionActionEvent):
@@ -95,6 +110,26 @@ class Starboard(commands.Cog):
                     )
                 else:
                     await self._add_to_starboard(message, len(reactions_with_star))
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, event: discord.RawReactionActionEvent):
+        emoji = event.emoji
+
+        if emoji.is_unicode_emoji() and str(emoji) == starboard_emoji:
+            channel = self.bot.get_channel(event.channel_id)
+            message = await channel.fetch_message(event.message_id)
+            reactions_with_star = [
+                i for i in message.reactions if i.emoji == starboard_emoji
+            ]
+
+            star_count = len(reactions_with_star)
+
+            starred_message = await self.collection.find_one({"message_id": message.id})
+
+            if star_count < 1:
+                return await self._remove_from_starboard(starred_message, message)
+
+            await self._update_starboard(starred_message, message, star_count)
 
 
 def setup(bot: ModmailBot):
